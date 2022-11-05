1eacf7af71434229f8e64d1000856d4963156ee8
JulicaKuz <jkuz@christianvenzke.de>
2022-11-05 11:56:12 +0100

    implementation count active cells

--- gameoflife/babysteps/GameOfLife.py
+++ gameoflife/babysteps/GameOfLife.py
@@ -1,2 +1,9 @@
 def countActiveNeighbours(field):
-    return 1
+    counter = 0
+    for r_index, row in enumerate(field):
+        for c_index, cell in enumerate(row):
+            if r_index == 1 and c_index == 1:
+                continue
+            elif cell is True:
+                counter += 1
+    return counter
